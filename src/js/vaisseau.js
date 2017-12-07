a9951b32f4a3b46f965c8fa2803198821891968a
Elercia <rabierv@hotmail.fr>
2017-12-07 20:26:56 +0100

    Add default struct

---
+++ src/js/vaisseau.js
@@ -0,0 +1,49 @@
+
+class GameObject {
+	constructor(x, y, h, w) {
+		this.x = x;
+		this.y = y;
+
+		this.h = h;
+		this.w = w;
+	}
+
+	draw() {
+		rect(x,y,w,h);
+	}
+
+	colideWith(other) {
+		if(this.x >= other.x && this.x<=(other.x+other.w)
+			&& this.y >= other.y && this.y<=(other.y+other.h)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	updatePos() {
+		// nothing to do here
+		// override this to make the magic append
+	}
+}
+
+
+class Vaisseau extends GameObject {
+	constructor(x, y, h, w){
+		super(x, y, h, w);
+	}
+} 
+
+
+class Ennemie extends GameObject {
+	constructor(x, y, h, w){
+		super(x, y, h, w);
+	}
+}
+
+
+class Missile extends GameObject {
+	constructor(x, y, h, w){
+		super(x, y, h, w);
+	}
+}
