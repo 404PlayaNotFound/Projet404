5a9f0c37eb71141f26b6963854c185f502f6037d
Btrd29 <btrd29@hotmail.fr>
2017-12-07 20:28:09 +0100

    main.js modify

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
 function setup() {
-  // put setup code here
+	createCanvas(800, 600);
+	background(256);
+}
+
+function keyPressed() {
+
+	if(keyCode==32) {
+  		background(25);
+	}
+
+	return false;
 }
 
 function draw() {
-  // put drawing code here
+  	// put drawing code here
+	keyPressed();
 }
